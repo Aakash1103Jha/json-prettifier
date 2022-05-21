9a1ce7a7d651cdb8aa70dc08ffef1da7aae49c38
Aakash Jha <aakash.jha1103@gmail.com>
2022-05-22 02:49:47 +0530

    Added scripts for CI/CD

--- server/utils/app.ts
+++ server/utils/app.ts
@@ -3,18 +3,24 @@ import cors from "cors"
 import { resolve } from "path"
 
 const app: Application = express()
+const { MODE } = process.env
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static(resolve(__dirname, "..", "..", "client", "build")))
 app.use(cors())
+
+if (MODE !== "dev") {
+	app.use(express.static(resolve(__dirname, "build")))
+} else {
+	app.use(express.static(resolve(__dirname, "..", "..", "client", "build")))
+}
 
 app.get("/api", async (req: Request, res: Response) => {
 	res.status(200).send("Welcome!")
 })
 
 app.use("*", async (req: Request, res: Response) => {
-	res.sendFile(resolve(__dirname, "..", "..", "client", "build", "index.html"))
+	res.sendFile(resolve(__dirname, "build", "index.html"))
 })
 
 export default app
